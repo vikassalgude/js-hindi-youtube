6a75decabfcc7bab6df2320b45de8c201aac3027
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-04-22 13:26:51 +0000

    container config done

---
+++ 01_basic/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=1445553
+let accountEmail="hitesh@google.com"
+var accountPassword="12345"
+accountCity="Jaipur"
+// accountId=12 //comment
+accountEmail="njdxej @djk.com"
+accountPassword="899u7809"
+accountCity="mumbai"//var
+let accountState;// value is undefined
+
+console.log(accountId)
+console.table([accountEmail,accountPassword,accountCity,accountState])
+// if (condition) {
+    
+// }}
+/*
+Prefer not to use var bcoz of issue in block and functional scope
+*/
