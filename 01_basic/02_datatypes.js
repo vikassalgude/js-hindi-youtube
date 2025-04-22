115144e7ea2dcbd94f62ea4a139dd9d1ef62d210
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-04-22 13:47:41 +0000

    datatype

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";//treat all JS code as newer version
+// alert(2+3) we r using node js,not browser
+
+// console.log(3+3) console.log("hitesh") error
+console.log(3+3) ;console.log("hitesh")  //will reduce readability
+let name="vikas"
+let age=22
+let isLoggedIn=false
+
+//number =>2 to power 53
+//bigint
+//string=>" "or ''
+//boolean =>true or false
+//null=> standalone value representation of null value
+//undefined =>
+// symbol=>
+
+//object
+console.log(typeof name);
+console.log(typeof null);
+console.log(typeof undefined);
+
+
+
+
+
+
