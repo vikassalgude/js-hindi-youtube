425e2716e70f5c554de74d7b65fec9c893966d18
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-04-24 14:10:37 +0000

    conversion of datatypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,24 @@
+// let score="33abc"
+// let score=null
+// let score=undefined
+
+
+// // const{score} = req.body
+// console.log(typeof score)
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber)
+// console.log( valueInNumber)
+//"33"=>33
+//"33abc"=>NaN not a number
+//true=>1 fasle=>0
+// // let isLoggedIn="";//false
+// let isLoggedIn="jhdei";//true
+
+
+// let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn)
+
+let a=33
+let s=String(a)
+console.log(typeof s)
+
