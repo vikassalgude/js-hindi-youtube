b707ebfde75692e691112c6beb715456279aab68
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-04-24 17:36:39 +0000

    string

---
+++ 01_basic/03_string.js
@@ -0,0 +1,18 @@
+let str1="vikas"
+let str2="salgude"
+let str3=str1+str2
+// console.log(str3)
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+let num1 ,num2 ,num3
+num1=num2=num3=2+2
+let game=12
+game++
+console.log(game)
+++game
+console.log(game)
+
+
+
