fa9676b097381e7006a7076858fd80b9eb59c448
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-04-25 23:48:20 +0530

    Create datatypes-summary.js

---
+++ 01_basic/datatypes-summary.js
@@ -0,0 +1,49 @@
+ "use strict"// treat all JS code as newer version
+// //primmitive data types
+// // 7 types string number boolean null undefined symbol BigInt
+// const score=22
+// const score01=22.266
+// const isLoggedIn=false;
+// const outside=null
+// let userEmail;
+// const id=Symbol("123")
+// const id1=Symbol("123")
+// const bignumber=4088174898437895783n;
+// console.log(bignumber)
+// //reference (non primitive)
+// //array object functions
+// const heroes=["shaktiman","batman","marco"]
+// let myobj={
+//     name:"vikas",
+//     age:22
+// }
+// const myfunc=function(){
+//     console.log("hello world")
+// }
+// console.log(typeof myfunc)
+// console.log(typeof bignumber)
+// console.log(typeof myobj)
+// console.log(typeof heroes)
+
+
+// **********lect10************
+// stack(primitive) heap(non-primitive)
+let name1="vikassalgude"
+let name2=name1
+
+
+name2="xyz"
+console.log(name1)
+console.log(name2)
+let my={
+    email:"vghvk",
+
+}
+
+let ur=my
+ur.email="njin"
+console.log(my.email)
+console.log(ur.email)
+
+
+
