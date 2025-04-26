a10f58ca07ef875733681bf27f8b1766f204bb79
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-04-26 18:27:49 +0000

    strings

---
+++ 01_basic/05_strings.js
@@ -0,0 +1,25 @@
+const name="hitesh"
+const repocount=5;
+// console.log(name+" "+repocount+"value")
+// console.log('Hello my name is ${name} and my repocount is ${repocount}');
+console.log(`Hello my name is ${name} and my repocount is ${repocount}`);
+const gamename=new String("jow-cl")
+console.log(gamename[0])
+console.log(gamename.__proto__)
+console.log(gamename.length)
+console.log(gamename.toUpperCase())
+console.log(gamename.charAt(3))
+console.log(gamename.indexOf('l'))
+// const newstring=gamename.substring(0,4)
+// console.log(newstring);
+// const newstring=gamename.slice(-6,2)
+// console.log(newstring);
+const newstring="         kmlskmpmep  mdlm   "
+
+console.log(newstring.trim());
+console.log(newstring.replace("mdlm","kmo"));
+console.log(newstring.includes("mdlm"));
+
+console.log(newstring.split(" "));
+
+
