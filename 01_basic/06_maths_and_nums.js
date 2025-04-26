7f0ba1f634183dc45ae7628981933221cba58174
VIKAS SALGUDE <142891781+vikassalgude@users.noreply.github.com>
2025-04-26 19:29:13 +0000

    maths ans nums

---
+++ 01_basic/06_maths_and_nums.js
@@ -0,0 +1,25 @@
+// const score=400
+// const balance=new Number(1000)
+// console.log(balance);
+// console.log(score);
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+// const other=23.8934
+// console.log(other.toPrecision(3));
+// const hundred=1000000000
+// console.log(hundred.toLocaleString('en-IN'));
+// *****maths**********
+// console.log(Math);
+// console.log(Math.abs(-33));
+// console.log(Math.round(2.534));
+// console.log(Math.ceil(3.34));
+// console.log(Math.floor(3.312));
+// console.log(Math.pow(32,23));
+// console.log(Math.sqrt(13314));
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1)+min)+10);
